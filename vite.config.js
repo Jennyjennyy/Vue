6a21f3a2e7e11c73ac6897a3b8d45a48138db17f
冯子航 <fengzihang@Fengs-MacBook-Pro.local>
2025-08-01 10:21:37 +0800

    feat: xx

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: true,  // 开放所有本地IP
-    port: 5173,  // 你可以设置你自己的端口号
+    host: true,
+    port: 5173,  // 如果需要，你可以设置你自己的端口
     proxy: {
       '/api': {
-        target: 'http://123.58.56.61',  // 你的后端 API 地址
-        changeOrigin: true,  // 将请求的 Origin 修改为目标地址，避免 CORS 问题
-        rewrite: (path) => path.replace(/^\/api/, ''),  // 如果后端路径不需要 /api 前缀
+        target: process.env.VITE_API_URL,  // 设置目标服务器的地址
+        changeOrigin: true,  // 改变请求头中的 Origin，避免 CORS 问题
+        rewrite: (path) => path.replace(/^\/api/, ''),  // 去掉 /api 前缀
       },
     },
   },
